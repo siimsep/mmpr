facdc69ce0493ea3b4287d685288d2222c208041
siimsep <siimsep@tlu.ee>
2022-03-04 14:27:22 +0200

    raskustasemeheleht

--- app.js
+++ app.js
@@ -5,10 +5,29 @@ const avaleht = document.getElementById('avaleht');
 const loosiratas = document.getElementById('loosiratas');
 const loosirattaNupp = document.getElementById('loosirattanupp');
 const kysimuseleht = document.getElementById('kysimuseleht');
+/////////////////////////////////////////////////////////////////////////
+// Raskustaseme valimine 
+const raskustase = document.getElementById('raskustasemevaade');
+const noobButton = document.getElementById('noob');
+const expertButton = document.getElementById('expert');
+let raskus = 1; //default 1, kui k6rgem tase siis muutub 2ks
+noobButton.addEventListener('click', function() {
+    raskustase.classList.add('hide')
+    loosirattavaade();
+});
+expertButton.addEventListener('click', function() {
+    raskustase.classList.add('hide');
+    loosirattavaade();
+    raskus = 2; 
+});
+/////////////////////////////////////////////////////////////////////////
 
 
 function yksikM2ng() {
     avaleht.classList.add('hide');
+    raskustase.classList.remove('hide');
+}
+function loosirattavaade() {
     loosiratas.classList.remove('hide');
 }
 function kysimus() {
