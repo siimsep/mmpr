2922bb5fba67fe0f3850aafcb42376ee56f1f8e8
siimsep <siimsep@tlu.ee>
2022-03-07 11:38:11 +0200

    vigane hetkel veits

--- app.js
+++ app.js
@@ -27,12 +27,23 @@ function yksikM2ng() {
     avaleht.classList.add('hide');
     raskustase.classList.remove('hide');
 }
+let kategooriaNr = 1;
 function loosirattavaade() {
     loosiratas.classList.remove('hide');
+    const valik1 = document.getElementById('valik1');
+    const valik2 = document.getElementById('valik2');
+    valik1.addEventListener('click', function() {
+        kategooriaNr = valik1.value;
+        kysimus();
+    })
+    valik2.addEventListener('click', function() {
+        kategooriaNr = valik1.value;
+        kysimus();
+    })
 }
-function kategooriaPopup() {
+/* function kategooriaPopup() {
     document.getElementById('kategooriaPopup').classList.add('hide');
-}
+} */
 function kysimus() {
     loosiratas.classList.add('hide');  
     //setTimeout('kategooriaPopup()', 5000);  
@@ -42,6 +53,7 @@ function kysimus() {
     document.getElementById('variant2').innerHTML=massiiv[2];
     document.getElementById('variant3').innerHTML=massiiv[3];
     document.getElementById('variant4').innerHTML=massiiv[4];
+    getFromFirebase();
 
 }
 function mineAvalehele() { // v6iks kuidagi paremini selle nullimise teha, esialgu nii
@@ -114,8 +126,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
-const kataloog = ref(db, "kyssad/2/");
-
+//const kataloog = ref(db, "kyssad/2/");
+//const kataloog = ref(db, `kyssad/${kategooriaNr}/`);
 /////////////////////////////////////////////////////////////////////////
 // Kasutaja skoor Firebase andmebaasi
 function kirjutaEdetabelisse(idnr,nickName, score, raskusTase) {
@@ -142,7 +154,29 @@ set(child(uusKyss, "nF"), nF);
 // Firebasest kysimuse lugemine
 var massiiv = [];
 let rightAnswer;
-onValue(kataloog, gotData);               // Firebasest lugemise funktsioon
+/* onValue(kataloog, gotData);               // Firebasest lugemise funktsioon
+function gotData(data)  {                 // Firebasest saadud objekti töötlemine
+let info = data.val();                  // Firebase snapshotist (data) Javascript objektiks muutmine (.val funktsioon)
+let keys = Object.keys(info);           // JS Objekti (info) töötlemine massiiviks (keys)
+for (let i =0;i<keys.length;i++){       // Massiivi läbikäimine
+let r = keys[i];
+massiiv.push(info[r].kyss);
+massiiv.push(info[r].v1);
+massiiv.push(info[r].v2);
+massiiv.push(info[r].v3);
+massiiv.push(info[r].v4);
+massiiv.push(info[r].nF);
+rightAnswer = massiiv[4];
+let questions = massiiv.slice(1,5);
+let randomizedQs = getShuffledArr(questions)
+massiiv[1]=randomizedQs[0];
+massiiv[2]=randomizedQs[1];
+massiiv[3]=randomizedQs[2];
+massiiv[4]=randomizedQs[3];
+}} */
+const getFromFirebase = async () => {
+    const kataloog = ref(db, `kyssad/${kategooriaNr}/`);
+    await onValue(kataloog, gotData);               // Firebasest lugemise funktsioon
 function gotData(data)  {                 // Firebasest saadud objekti töötlemine
 let info = data.val();                  // Firebase snapshotist (data) Javascript objektiks muutmine (.val funktsioon)
 let keys = Object.keys(info);           // JS Objekti (info) töötlemine massiiviks (keys)
@@ -162,6 +196,7 @@ massiiv[2]=randomizedQs[1];
 massiiv[3]=randomizedQs[2];
 massiiv[4]=randomizedQs[3];
 }}
+}
 /////////////////////////////////////////////////////////////////////////
 // Firebasest tulnud kysimuse vastuste randomizer
 const getShuffledArr = arr => {
@@ -173,15 +208,69 @@ const getShuffledArr = arr => {
     return newArr
 };
 /////////////////////////////////////////////////////////////////////////
+// vastusenuppude sissetoomine
+const v1btn = document.getElementById('variant1');
+const v2btn = document.getElementById('variant2');
+const v3btn = document.getElementById('variant3');
+const v4btn = document.getElementById('variant4');
+const nextQ = document.getElementById('nextQuest');
+/////////////////////////////////////////////////////////////////////////
+// Vastamise korral vastuste nuppude deaktiveerimine
+const btnDisabler = ()=> {
+    v1btn.disabled = true;
+    v2btn.disabled = true;
+    v3btn.disabled = true;
+    v4btn.disabled = true;
+}
+/////////////////////////////////////////////////////////////////////////
+// Uue kyssa korral vastuste nuppude aktiveerimine
+const btnEnabler = ()=> {
+    v1btn.disabled = false;
+    v2btn.disabled = false;
+    v3btn.disabled = false;
+    v4btn.disabled = false;
+    v1btn.style.cssText = ""; // et vastatud nupu v2rvist lahti saada! 
+    v2btn.style.cssText = ""; // loll lahendus, aga praegu t88tab nii
+    v3btn.style.cssText = ""; 
+    v4btn.style.cssText = ""; 
+}
+/////////////////////////////////////////////////////////////////////////
 // Vastatud kysimuse v6rdlus
-const checkAnswer = function(event){
+const checkAnswer = (event) =>{
     let vastatudVariant = event.target.innerHTML;
     if(vastatudVariant == rightAnswer) {
-        console.log('paistab ilus');
-    } else { alert('vale')}
- }
-document.getElementById('variant1').addEventListener('click', checkAnswer);
-document.getElementById('variant2').addEventListener('click', checkAnswer);
-document.getElementById('variant3').addEventListener('click', checkAnswer);
-document.getElementById('variant4').addEventListener('click', checkAnswer);
+        alert('õige'); 
+        event.target.style.cssText = "background-color: green;"; 
+        showNerdFact();
+        btnDisabler();
+        nextQ.classList.remove('hide');
+
+    } else { 
+        alert('vale');
+        event.target.style.cssText = "background-color: red;";
+        btnDisabler();
+        nextQ.classList.remove('hide');
+    };
+}
+v1btn.addEventListener('click', checkAnswer);
+v2btn.addEventListener('click', checkAnswer);
+v3btn.addEventListener('click', checkAnswer);
+v4btn.addEventListener('click', checkAnswer);
+const uueleRingile = () => {
+    btnEnabler();
+    kysimuseleht.classList.add('hide');
+    nerdFactContainer.classList.add('hide');
+
+    loosirattavaade();
+}
+nextQ.addEventListener('click', uueleRingile);
+
+/////////////////////////////////////////////////////////////////////////
+// Nedfacti kuvamine
+const nerdFactContainer = document.getElementById('nerdFactContainer');
+const nF = document.getElementById('nF');
+const showNerdFact = () => {
+    nerdFactContainer.classList.remove('hide');
+    nF.innerHTML = "Nerdfact: " + massiiv[5];
+}
 /////////////////////////////////////////////////////////////////////////
